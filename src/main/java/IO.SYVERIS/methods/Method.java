683d7489fa0756d0992935dc8d319b51cc4ad90f
Affan Orakzai <affanorakzai@syveris.us.kg>
2024-10-25 17:40:07 +0500

    Deleted unnecessary files.

--- src/main/java/IO.SYVERIS/methods/Method.java
+++
@@ -1,8 +0,0 @@
-package IO.SYVERIS.methods;
-
-import IO.SYVERIS.utils.ProxyLoader;
-import io.netty.channel.Channel;
-import java.util.function.BiConsumer;
-
-public interface Method extends BiConsumer<Channel, ProxyLoader.Proxy> {
-}
